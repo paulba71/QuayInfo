1deb6267b1f550afc86f9d13a5d5e468a124aacc
Paul Barnes <paulba71@hotmail.com>
2025-03-01 14:29:29 +0000

    Initial Commit

---
+++ QuayInfo/QuayInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuayInfoApp.swift
+//  QuayInfo
+//
+//  Created by Paul Barnes on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuayInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuayInfoTests/QuayInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuayInfoTests.swift
+//  QuayInfoTests
+//
+//  Created by Paul Barnes on 01/03/2025.
+//
+
+import Testing
+@testable import QuayInfo
+
+struct QuayInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
